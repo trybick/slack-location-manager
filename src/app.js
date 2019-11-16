5344b57dbfca4ba9fa54128d93bd3cd2d126b150
trybick <t.rybicki@gmail.com>
2019-11-16 18:00:10 -0500

    Set a custom status

--- src/app.js
+++ src/app.js
@@ -6,18 +6,27 @@ const web = new WebClient(process.env.SLACK_TOKEN);
 
 (async () => {
   const res = await web.auth.test();
-  console.log('res:', res);
   const userId = res.user_id;
 
-  // Post a message test
-  // await web.chat.postMessage({
-  //   channel: userId,
-  //   text: `The current time is ${currentTime}`
-  // });
-
-  // Get current status
-  // const currentStatus = await web.users.profile.get({});
-  // console.log('current status:', currentStatus);
+  // Set custom status
+  const custom = await web.users.profile.set({
+    profile: {
+      status_text: 'riding a train',
+      status_emoji: ':mountain_railway:',
+      status_expiration: 0
+    }
+  });
+  console.log('custom:', custom);
 
   console.log('Done!');
 })();
+
+// Post a message test
+// await web.chat.postMessage({
+//   channel: userId,
+//   text: `The current time is ${currentTime}`
+// });
+
+// Get current status
+// const currentStatus = await web.users.profile.get({});
+// console.log('current status:', currentStatus);
