b4fe8aef9bc064d58d2a64c2b35f517d13e33b63
trybick <t.rybicki@gmail.com>
2019-11-16 17:20:53 -0500

    Create app skeleton

---
+++ app.js
@@ -0,0 +1,23 @@
+const { WebClient } = require('@slack/web-api');
+
+console.log('Getting started with Node Slack SDK');
+
+const web = new WebClient(process.env.SLACK_TOKEN);
+// The current date
+const currentTime = new Date().toTimeString();
+
+(async () => {
+  // Use the `auth.test` method to find information about the installing user
+  const res = await web.auth.test()
+
+  // Find your user id to know where to send messages to
+  const userId = res.user_id
+
+  // Use the `chat.postMessage` method to send a message from this app
+  await web.chat.postMessage({
+    channel: userId,
+    text: `The current time is ${currentTime}`,
+  });
+
+  console.log('Message posted!');
+})();
