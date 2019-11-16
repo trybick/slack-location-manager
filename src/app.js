3f7a27bf0a5e4d15f4d29471fb8c667a23b30067
trybick <t.rybicki@gmail.com>
2019-11-16 17:51:22 -0500

    Trim excess

--- src/app.js
+++ src/app.js
@@ -2,13 +2,9 @@ const { WebClient } = require('@slack/web-api');
 const dotenv = require('dotenv');
 
 dotenv.config();
-
 const web = new WebClient(process.env.SLACK_TOKEN);
 
-const currentTime = new Date().toTimeString();
-
 (async () => {
-  // Use the `auth.test` method to find information about the installing user
   const res = await web.auth.test();
   console.log('res:', res);
 
