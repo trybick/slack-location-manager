18abe195cb758df6e034236fab303f34c829ab73
trybick <t.rybicki@gmail.com>
2019-11-16 17:35:46 -0500

    Successfully post message

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
 const { WebClient } = require('@slack/web-api');
 const dotenv = require('dotenv');
 
-console.log('Getting started with Node Slack SDK');
+dotenv.config();
 
 const web = new WebClient(process.env.SLACK_TOKEN);
-// The current date
+
 const currentTime = new Date().toTimeString();
 
 (async () => {
@@ -12,10 +12,8 @@ const currentTime = new Date().toTimeString();
   const res = await web.auth.test();
   console.log('res:', res);
 
-  // Find your user id to know where to send messages to
   const userId = res.user_id;
 
-  // Use the `chat.postMessage` method to send a message from this app
   await web.chat.postMessage({
     channel: userId,
     text: `The current time is ${currentTime}`
