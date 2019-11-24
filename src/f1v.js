0fd56a5e7c663aecc8fd78bc44d4898559fe22f4
trybick <t.rybicki@gmail.com>
2019-11-23 19:45:17 -0500

    Get and save logs

--- src/f1v.js
+++ src/f1v.js
@@ -1,15 +1,24 @@
-
 const { WebClient } = require('@slack/web-api');
 const dotenv = require('dotenv');
+const fs = require('fs');
 
 dotenv.config();
 const web = new WebClient(process.env.F1V_TOKEN);
 
-(async () => {
-  const res = await web.auth.test();
-  // console.log('res:', res)
+const saveDataToFile = data => {
+  const path = `./logs/${new Date().toJSON()}.json`;
+  fs.writeFile(path, data, err => {
+    if (err) throw err;
+    console.log('The file was saved to:', path);
+  });
+};
 
+(async () => {
   // Get logs
-  const logs = await web.team.accessLogs();
-  console.log('Logs: ', logs);
+  await web.team
+    .accessLogs()
+    .then(res => {
+      saveDataToFile(JSON.stringify(res, null, 2));
+    })
+    .catch(err => console.log('Error:', err));
 })();
