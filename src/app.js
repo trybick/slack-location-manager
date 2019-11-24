fe4f208ab8a6da281e6f188a61a6084e0d04a9b5
trybick <t.rybicki@gmail.com>
2019-11-23 19:04:03 -0500

    Add error handling to app file

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ const web = new WebClient(process.env.CELTIC_TOKEN);
   const userId = res.user_id;
 
   // Set custom status
-  const newStatus = await web.users.profile.set({
-    user: userId,
-    profile: {
-      status_text: 'riding a bus',
-      status_emoji: ':mountain_railway:',
-      status_expiration: 0
-    }
-  });
-  console.log('New status set to:', newStatus.profile.status_text);
+  await web.users.profile
+    .set({
+      user: userId,
+      profile: {
+        status_text: 'riding a bus',
+        status_emoji: ':mountain_railway:',
+        status_expiration: 0
+      }
+    })
+    .then(res => console.log('Response:', res)
+    .catch(err => console.log('Error:', err.data.error));
 })();
