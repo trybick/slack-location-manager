881a9490648bc326eab96e81600eb1dbe3fda205
trybick <t.rybicki@gmail.com>
2019-12-05 20:27:28 -0500

    WIP on assigning IP address to variable from request call

--- src/lib/setStatus.js
+++ src/lib/setStatus.js
@@ -1,34 +1,43 @@
 const { WebClient } = require('@slack/web-api');
 const dotenv = require('dotenv');
+const request = require('request');
+const handleErrors = require('../util/handleErrors');
 
 //
 // This is the script that cron will call repeatedly
 //
 
-
 // Todo:
 // Add IP address conditions for status
+
+// Import token from local storage
+// const ip=$(curl -s https://ipinfo.io/ip)
+// const city=$(curl -s https://ipvigilante.com/${ip} | /usr/local/bin/jq ".data.city_name")
+
+const ip = request('https://ipinfo.io/ip', function(error, response, body) {
+  if (error) {
+    handleErrors(error);
+  }
+
+  return body;
+});
 
 dotenv.config();
 const web = new WebClient(process.env.CELTIC_USER_2_TOKEN); // Token determines the user
 
-// Import token from local storage
-const ip=$(curl -s https://ipinfo.io/ip)
-const city=$(curl -s https://ipvigilante.com/${ip} | /usr/local/bin/jq ".data.city_name")
+// (async () => {
+//   const res = await web.auth.test();
+//   const userId = res.user_id;
 
-(async () => {
-  const res = await web.auth.test();
-  const userId = res.user_id;
-
-  await web.users.profile
-    .set({
-      user: userId,
-      profile: {
-        status_text: 'boom',
-        status_emoji: ':mountain_railway:',
-        status_expiration: 0
-      }
-    })
-    .then(res => console.log('Response:', res))
-    .catch(err => console.log('Error:', err.data.error));
-})();
+//   await web.users.profile
+//     .set({
+//       user: userId,
+//       profile: {
+//         status_text: 'boom',
+//         status_emoji: ':mountain_railway:',
+//         status_expiration: 0
+//       }
+//     })
+//     .then(res => console.log('Response:', res))
+//     .catch(err => console.log('Error:', err.data.error));
+// })();
