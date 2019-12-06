79f8685ecd12d4046882fbdacbc705232044b45a
trybick <t.rybicki@gmail.com>
2019-12-05 20:13:34 -0500

    Fix node_modules ignore

--- src/lib/setStatus.js
+++ src/lib/setStatus.js
@@ -1,14 +1,25 @@
 const { WebClient } = require('@slack/web-api');
 const dotenv = require('dotenv');
 
+//
+// This is the script that cron will call repeatedly
+//
+
+
+// Todo:
+// Add IP address conditions for status
+
 dotenv.config();
-const web = new WebClient(process.env.CELTIC_USER_2_TOKEN); // my private slack workspace
+const web = new WebClient(process.env.CELTIC_USER_2_TOKEN); // Token determines the user
+
+// Import token from local storage
+const ip=$(curl -s https://ipinfo.io/ip)
+const city=$(curl -s https://ipvigilante.com/${ip} | /usr/local/bin/jq ".data.city_name")
 
 (async () => {
   const res = await web.auth.test();
   const userId = res.user_id;
 
-  // Set custom status (personal workspace)
   await web.users.profile
     .set({
       user: userId,
