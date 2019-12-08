463e0575c6d73041b07334c5866ddeb33cf4f75d
trybick <t.rybicki@gmail.com>
2019-12-07 20:17:29 -0500

    Add success cron message

--- src/install/createCron.js
+++ src/install/createCron.js
@@ -4,21 +4,21 @@ const crontab = require('crontab');
 // Schedule a system cron job for the recurring script
 //
 
-async function createCronJob(time) {
+function createCronJob(time) {
   const formattedTime = time // Parse '8:45' into '45 8'
     .split(':')
     .reverse()
     .join(' ');
 
   const schedule = `${formattedTime} * * 1-5`;
-  console.log(await 'Please click OK to create a cron job');
 
-  await crontab.load(function(err, job) {
+  crontab.load(async function(err, job) {
     job.create('ls -la', schedule);
     job.save();
-  });
 
-  console.log(await `Cron job created: ${time} Mon-Fri`);
+    console.log(await `Cron job created for ${time} Monday - Friday.`);
+    console.log(await `Please click OK for write access if prompted.`);
+  });
 }
 
 module.exports = createCronJob;
