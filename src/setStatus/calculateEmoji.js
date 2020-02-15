081e1e2bf2e1bf09eb07caf0d7d2daec57de80c1
Tim Rybicki <t.rybicki@gmail.com>
2020-02-14 20:04:22 -0500

    fix(emoji): Add SSID scanning to false results in office

--- src/setStatus/calculateEmoji.js
+++ src/setStatus/calculateEmoji.js
@@ -1,27 +1,50 @@
+/* eslint-disable no-restricted-syntax */
 const rp = require('request-promise');
 const handleErrors = require('../util/handleErrors');
-const geolib = require('geolib');
+const wifi = require('node-wifi');
 
 //
-// Return emoji dependent on user's IP address location
+// Return emoji dependent on user's location
 //
 
 const emojiMap = {
-  f1v: null,
   remote: ':house_with_garden:',
   billerica: ':yoop-jazz-hands:',
   concord: ':yoop-jazz-hands:',
 };
 
 async function calculateEmoji() {
-  const { city, gps } = await _getIpInfo();
-  const isPossiblyAtF1V = city === 'waltham' || city === 'watertown';
-
-  if (isPossiblyAtF1V && _checkGPSCoords(gps) < 1) {
-    return emojiMap.f1v;
+  if (await _calcIsAtF1v()) {
+    return null;
   }
 
+  const { city } = await _getIpInfo();
+
   return emojiMap[city] || emojiMap.remote;
+}
+
+// Can't rely on IP address location at F1V (sometimes it comes back as 'Boston')
+// Instead, look for a match in the wifi SSIDs
+function _calcIsAtF1v() {
+  wifi.init({
+    iface: null,
+  });
+
+  return wifi
+    .scan()
+    .then(networks => {
+      let isAtF1v = false;
+
+      for (const network of networks) {
+        if (network.ssid.includes('Divide_By_Zero') || network.ssid.includes('Novatio')) {
+          isAtF1v = true;
+          break;
+        }
+      }
+
+      return isAtF1v;
+    })
+    .catch(handleErrors);
 }
 
 function _getIpInfo() {
@@ -36,22 +59,6 @@ function _getIpInfo() {
       return { city, gps };
     })
     .catch(handleErrors);
-}
-
-function _checkGPSCoords(gps) {
-  const coords = gps.split(',');
-  const F1V = {
-    lat: '42.366716',
-    long: '-71.218910',
-  };
-
-  const dist = geolib.getDistance(
-    { latitude: coords[0], longitude: coords[1] },
-    { latitude: F1V.lat, longitude: F1V.long }
-  );
-  const distInMiles = dist / 1609;
-
-  return distInMiles;
 }
 
 module.exports = calculateEmoji;
